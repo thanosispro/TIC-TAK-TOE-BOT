7122d84ab1bf62a15360e352782a74a1ecfbd772
thanosispro <126332750+thanosispro@users.noreply.github.com>
2023-12-08 03:34:31 -0800

    Update real.js

--- real.js
+++ real.js
@@ -10,18 +10,21 @@ let winboard=[
 ];
 console.log(winboard[7][2]);
 let turn=document.getElementById("turn").innerHTML;
-let i,j,k,m,gameboard=[],count=1,test=0,random,st=1,lol=1;
+let i,a,b,c,j,k,m,gameboard=[],count=1,test=0,random,st=1,lol=1,name;
 for(i=0;i<=8;i++){
     gameboard[i]=document.getElementsByClassName("cell")[i];
 }
+
 function startgame(){
+    
     if(document.getElementById("input").value===""){
         alert("please fill input filed");
     }
     else{
 st=0;
 document.getElementById("turn").innerHTML=`${document.getElementById("input").value} VS Parlee Khadka`;
-    }
+name=document.getAnimations("input").value;    
+}
 }
 function restartgame(){
     lol++;
@@ -42,21 +45,30 @@ setTimeout(function(){gameboard[0].innerHTML="O";},200); }
 
 function checkwin(){
     for(i=0;i<=7;i++){
+        
 if(gameboard[winboard[i][0]].innerHTML==="O" && gameboard[winboard[i][1]].innerHTML==="O" && gameboard[winboard[i][2]].innerHTML==="O" || gameboard[winboard[i][0]].innerHTML==="X" &&gameboard[winboard[i][1]].innerHTML==="X" &&gameboard[winboard[i][2]].innerHTML==="X"){
     if(gameboard[winboard[i][0]].innerHTML==="X"){
         document.getElementById("checkwin").innerHTML=`the winner is ${document.getElementById("input").value}`;}
     else{
         document.getElementById("checkwin").innerHTML=`the winner is Parlee Dai`;
     }
-   
+
 document.getElementById("restart").innerHTML=`<button onclick="restartgame()" id="btn">Restart</button>`;
 document.getElementById("start").innerHTML="";
+break;
 }
 else{
+    a=gameboard[winboard[i][0]].innerHTML;
+    b=gameboard[winboard[i][1]].innerHTML;
+    c=gameboard[winboard[i][2]].innerHTML;
+
+    console.log(a,b,c,i);
     if(i==7 && gameboard[0].innerHTML!=="" && gameboard[3].innerHTML!=="" &&  gameboard[1].innerHTML!=="" &&  gameboard[2].innerHTML!=="" && gameboard[4].innerHTML!=="" && gameboard[5].innerHTML!=="" && gameboard[6].innerHTML!=="" && gameboard[7].innerHTML!=="" && gameboard[8].innerHTML!=="" ){
-    document.getElementById("checkwin").innerHTML=`The Game is Draw`;
+       
+        document.getElementById("checkwin").innerHTML=`The Game is Draw`;
     document.getElementById("restart").innerHTML=`<button onclick="restartgame()" id="btn">Restart</button>`;
     document.getElementById("start").innerHTML="";
+    break;
 }
 }
 
@@ -64,6 +76,7 @@ else{
     }
 }
 function defend(){
+    
     test=0;
 for(i=0;i<=7;i++){
   
@@ -93,6 +106,7 @@ for(i=0;i<=7;i++){
                if(i===7){
                for(j=0;j<=100;j++){
                 random=Math.floor(Math.random() * 9);
+                console.log(random);
                    if(gameboard[random].innerHTML===""){
                     console.log("else");
                     console.log(j);
@@ -108,11 +122,12 @@ for(i=0;i<=7;i++){
                
            
     }
-checkwin();
+    checkwin();
 }
 
 
 function cells(that){
+   
     if(st==1){
         alert(" click on (start game)");}
         else{
@@ -171,8 +186,10 @@ function cells(that){
             
     }
    
-
+  
     
 }
 }
+
+
 
